6c41cddaf758e201fd5c450d3274db69fbc3e9ce
Hyunjoon <hkim778@uwo.ca>
2020-07-24 12:09:17 -0400

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-uploads-hj",
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://6ou9ihsn78.execute-api.us-east-1.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_P5pN2d7kS",
+    APP_CLIENT_ID: "4bslg9rnuoi3nqq55lrietk8mg",
+    IDENTITY_POOL_ID: "us-east-1:d26c4c41-c839-4def-8f98-36a0cf9b65eb",
+  },
+};
