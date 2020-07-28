8599dbb621c42ca5748763bddccc7dccaea59ab5
Hyunjoon <hkim778@uwo.ca>
2020-07-28 12:29:04 -0400

    Building our React App

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
   s3: {
     REGION: "us-east-1",
     BUCKET: "notes-app-uploads-hj",
@@ -11,6 +12,6 @@ export default {
     REGION: "us-east-1",
     USER_POOL_ID: "us-east-1_P5pN2d7kS",
     APP_CLIENT_ID: "4bslg9rnuoi3nqq55lrietk8mg",
-    IDENTITY_POOL_ID: "us-east-1:d26c4c41-c839-4def-8f98-36a0cf9b65eb",
+    IDENTITY_POOL_ID: "us-east-1:e4f3a0f0-da99-4a4a-a9da-693bbdb134a8",
   },
 };
